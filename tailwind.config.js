5a353eb97c7563a74c8828559e14ead38a7b6a95
nikhilganorkar <nikhilganorkar0@gmail.com>
2024-01-29 22:07:13 +0530

    Folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,25 @@ module.exports = {
         primaryDark: "#58E6D9", // 80,230,217
       }
     },
+    screens: {
+      "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
+  
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+  
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+  
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+  
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+  
+      xs: { max: "479px" },
+      // => @media (max-width: 479px) { ... }
+  },
   },
   plugins: [],
 }
